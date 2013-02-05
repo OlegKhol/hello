e282cb23e0d4a961054fc3353086687358f40159
Oleg Kholiavchyk <oleg.khol@gmail.com>
2013-02-05 22:27:10 +0200

    first

---
+++ hello/src/hello.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : Oleg Kholiavchyk
+// Version     :
+// Copyright   : (c) Oleg Kholiavchyk
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Final !" << endl; // prints Final !
+	return 0;
+}
